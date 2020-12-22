544bdb3b7e3ffc4c210884d6eacee3240a9e0e49
yasin <buntyvirani82@gmail.com>
2020-12-22 01:03:58 -0800

    destructured the data in object

--- covid-app/src/api/index.js
+++ covid-app/src/api/index.js
@@ -3,14 +3,9 @@ const url = "http://covid19.mathdro.id/api";
 
 export const fetchData = async () =>{
     try {
-        const {data} = await axios.get(url);
+        const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
 
-        const modifiedData = {
-            confirmed: data.confirmed,
-            recovered:data.recovered,
-            deaths:data.deaths,
-            lastUpdate:data.lastUpdate
-        }
+        const modifiedData = {confirmed,recovered,deaths,lastUpdate}
 
         return modifiedData;
         
