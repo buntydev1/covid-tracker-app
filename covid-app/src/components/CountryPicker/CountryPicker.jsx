a905977974ff35e80387610f64569bf85e5a7198
yasin <buntyvirani82@gmail.com>
2020-12-22 07:48:05 -0800

    applying option to form

--- covid-app/src/components/CountryPicker/CountryPicker.jsx
+++ covid-app/src/components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import {NativeSelect,FormControl} from '@material-ui/core';
 
 const CountryPicker = ()=>{
     return(
-        <h1>
-            CountryPicker
-        </h1>
+        <FormControl>
+            <NativeSelect>
+                <option value="global">Global</option>
+            </NativeSelect>
+        </FormControl>
     )
 }
 export default CountryPicker;
