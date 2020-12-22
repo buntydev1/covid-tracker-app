50d1049fec8535a32494c9934474495fe10e6e3d
yasin <buntyvirani82@gmail.com>
2020-12-22 04:49:15 -0800

    typography updated for date

--- covid-app/src/components/Cards/Cards.jsx
+++ covid-app/src/components/Cards/Cards.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import {Card,CardContent,Typograpy,Grid} from '@material-ui/core';
-import styles from './Cards.module.css';
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import styles from "./Cards.module.css";
 
-const Cards = (props)=>{
-    console.log(props);
-    return(
-        <div className={styles.container}>
-            <Grid spacing={3} justify="center">
-                <Grid item component={Card}>
-                    <CardContent>
-                        <Typograpy color="textSecondary" gutterBottom>
-                            Infected People
-                        </Typograpy>
-                        <Typograpy variant="h5">
-                            Real Data
-                        </Typograpy>
-                    </CardContent>
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+const Cards = (props) => {
+  console.log(props);
+  return (
+    <div className={styles.container}>
+      <Grid spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected People
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Real Date</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 export default Cards;
