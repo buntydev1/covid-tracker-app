4636b81fbefd19efe7acc8343c2e97e588e4fe33
yasin <buntyvirani82@gmail.com>
2020-12-22 00:26:23 -0800

    component updated

--- covid-app/src/App.js
+++ covid-app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Cards,Chart,CountryPicker} from './components';
 
 function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
+    <Cards />
+    <Chart />
+    <CountryPicker />
     </div>
   );
 }
---
+++ covid-app/src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CountryPicker = ()=>{
+    return(
+        <h1>
+            CountryPicker
+        </h1>
+    )
+}
+export default CountryPicker;
