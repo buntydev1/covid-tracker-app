0524c383b44ca8935135fd8b913882b322c86c5c
yasin <buntyvirani82@gmail.com>
2020-12-22 08:24:47 -0800

    mapping the fetch countires data and showing all in options

--- covid-app/src/components/CountryPicker/CountryPicker.jsx
+++ covid-app/src/components/CountryPicker/CountryPicker.jsx
@@ -17,6 +17,7 @@ const CountryPicker = ()=>{
         <FormControl className={styles.formControl}>
             <NativeSelect>
                 <option value="global">Global</option>
+    {fetchedCountries.map((country,i) =><option key={i}value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
     )
