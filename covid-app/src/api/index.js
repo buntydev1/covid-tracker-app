1328d9bea6af260dae8b4bd9b6d9d8430efe3e04
yasin <buntyvirani82@gmail.com>
2020-12-22 00:44:21 -0800

    using axios checking the response from url

--- covid-app/src/api/index.js
+++ covid-app/src/api/index.js
@@ -3,7 +3,8 @@ const url = "http://covid19.mathdro.id/api";
 
 export const fetchData = async () =>{
     try {
-        
+        const response = await axios.get(url);
+        console.log(response);
     } catch (error) {
         
     }
