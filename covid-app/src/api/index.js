543ae8a18b37c928ff6477965f03d1d18b6f4b8c
yasin <buntyvirani82@gmail.com>
2020-12-22 01:05:34 -0800

    taking the value and immediately returning it in object

--- covid-app/src/api/index.js
+++ covid-app/src/api/index.js
@@ -5,9 +5,9 @@ export const fetchData = async () =>{
     try {
         const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
 
-        const modifiedData = {confirmed,recovered,deaths,lastUpdate}
+        return {confirmed,recovered,deaths,lastUpdate};
 
-        return modifiedData;
+        
         
     } catch (error) {
         
